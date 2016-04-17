9ee637f1c4090ab0dab10fba37632e4659c09f0b
Ryan Bartley <ryanbartley@gmail.com>
2016-04-17 10:08:41 -0400

    Non-inline version of benchmark

--- src/Skeleton.h
+++ src/Skeleton.h
@@ -41,18 +41,7 @@ public:
 		
 		class JointClip {
 		public:
-			JointClip( const TransformClip &transform )
-			{
-				auto &transClip = transform.getTranslationClip();
-				auto &rotClip = transform.getRotationClip();
-				mStartTime = transClip.getStartTime();
-				mDuration = transClip.getDuration();
-				for( int i = 0, end = transClip.numKeyframes(); i < end; i++ ) {
-					auto translation = transClip.getKeyFrameValueAt( i );
-					auto rotation = rotClip.getKeyFrameValueAt( i );
-					jointAnim.emplace_back( translation.first, translation.second, rotation.second );
-				}
-			}
+			JointClip( const TransformClip &transform );
 			struct Value {
 				Value( float time, ci::vec3 trans, ci::quat rot )
 				: time( time ), trans( std::move( trans ) ),
@@ -62,52 +51,10 @@ public:
 				ci::quat	rot;
 			};
 			
-			ci::mat4 get( double absTime ) const
-			{
-				CI_ASSERT( jointAnim.size() >= 2 && jointAnim.size() >= 2 );
-				auto clamped = glm::clamp( absTime, mStartTime, mStartTime + mDuration );
-				auto begIt = begin( jointAnim );
-				auto nextIt = std::upper_bound( begIt, end( jointAnim ) - 1, clamped,
-				[]( float time, const Value &val ){
-					return time < val.time;
-				});
-				
-				auto prevIt = nextIt - 1;
-				auto lerped = lerp( *prevIt, *nextIt, clamped );
-				return getMatrix( lerped.first, lerped.second );
-			}
-			
-			ci::mat4 getLooped( double absTime ) const
-			{
-				CI_ASSERT( jointAnim.size() >= 2 && jointAnim.size() >= 2 );
-				auto cyclicTime = glm::mod( absTime, mDuration ) + mStartTime;
-				auto begIt = begin( jointAnim );
-				auto nextIt = std::upper_bound( begIt, end( jointAnim ) - 1, cyclicTime,
-				[]( float time, const Value &val ){
-					return time < val.time;
-				});
-				
-				auto prevIt = nextIt - 1;
-				auto lerped = lerp( *prevIt, *nextIt, cyclicTime );
-				return getMatrix( lerped.first, lerped.second );
-			};
-			
-			std::pair<ci::vec3, ci::quat> lerp( const Value &prev, const Value &next, float clampedTime ) const
-			{
-				std::pair<ci::vec3, ci::quat> ret;
-				auto perTime = (clampedTime - prev.time) / (next.time - prev.time);
-				ret.first = glm::mix( prev.trans, next.trans, perTime );
-				ret.second = glm::slerp( prev.rot, next.rot, perTime );
-				return ret;
-			}
-			
-			ci::mat4 getMatrix( const ci::vec3 &trans, const ci::quat &rot ) const
-			{
-				glm::mat4 ret;
-				ret *= glm::translate( trans );
-				ret *= glm::toMat4( rot );
-				return ret;
-			}
+			ci::mat4 get( double absTime ) const;
+			ci::mat4 getLooped( double absTime ) const;
+			std::pair<ci::vec3, ci::quat> lerp( const Value &prev, const Value &next, float clampedTime ) const;
+			ci::mat4 getMatrix( const ci::vec3 &trans, const ci::quat &rot ) const;
 			
 		private:
 			
@@ -123,18 +70,8 @@ public:
 			}
 		}
 		
-		void getLocal( double time, std::vector<ci::mat4> *localJointTransforms ) const
-		{
-			localJointTransforms->clear();
-			for( auto & jointClip : joints )
-				localJointTransforms->emplace_back( jointClip.get( time ) );
-		}
-		void getLoopedLocal( double time, std::vector<ci::mat4> *localJointTransforms ) const
-		{
-			localJointTransforms->clear();
-			for( auto & jointClip : joints )
-				localJointTransforms->emplace_back( jointClip.getLooped( time ) );
-		}
+		void getLocal( double time, std::vector<ci::mat4> *localJointTransforms ) const;
+		void getLoopedLocal( double time, std::vector<ci::mat4> *localJointTransforms ) const;
 		
 	private:
 		std::vector<JointClip> joints;
@@ -144,25 +81,7 @@ public:
 	public:
 		class JointClip {
 		public:
-			JointClip( const TransformClip &transform )
-			{
-				auto &transClip = transform.getTranslationClip();
-				auto &rotClip = transform.getRotationClip();
-				
-				mTransStartTime = transClip.getStartTime();
-				mTransDuration = transClip.getDuration();
-				for( int i = 0, end = transClip.numKeyframes(); i < end; i++ ) {
-					auto translation = transClip.getKeyFrameValueAt( i );
-					jointTrans.emplace_back( translation.first, translation.second );
-				}
-				
-				mRotStartTime = rotClip.getStartTime();
-				mRotDuration = rotClip.getDuration();
-				for( int i = 0, end = rotClip.numKeyframes(); i < end; i++ ) {
-					auto translation = rotClip.getKeyFrameValueAt( i );
-					jointRots.emplace_back( translation.first, translation.second );
-				}
-			}
+			JointClip( const TransformClip &transform );
 			
 			struct TransKeyframe {
 				TransKeyframe( float time, ci::vec3 trans )
@@ -178,74 +97,9 @@ public:
 				ci::quat	rot;
 			};
 			
-			ci::mat4 get( double absTime ) const
-			{
-				CI_ASSERT( jointTrans.size() >= 2 && jointRots.size() >= 2 );
-				auto transClamped = glm::clamp( absTime, mTransStartTime, mTransStartTime + mTransDuration );
-				auto rotClamped = glm::clamp( absTime, mRotStartTime, mRotStartTime + mRotDuration );
-				auto begItTrans = begin( jointTrans );
-				auto nextItTrans = std::upper_bound( begItTrans, end( jointTrans ) - 1, transClamped,
-				[]( float time, const TransKeyframe &val ){
-					return time < val.time;
-				});
-				
-				auto prevItTrans = nextItTrans - 1;
-				auto perTimeTrans = ( transClamped - prevItTrans->time) /
-									( nextItTrans->time - prevItTrans->time);
-				auto lerpTrans = glm::mix( prevItTrans->trans, nextItTrans->trans, perTimeTrans );
-				
-				auto begItRot = begin( jointRots );
-				auto nextItRot = std::upper_bound( begItRot, end( jointRots ) - 1, transClamped,
-				[]( float time, const RotKeyframe &val ){
-					return time < val.time;
-				});
-				
-				auto prevItRot = nextItRot - 1;
-				auto perTimeRot = ( rotClamped - prevItRot->time) /
-									( nextItRot->time - prevItRot->time);
-				auto lerpRot = glm::slerp( prevItRot->rot, nextItRot->rot, (float)perTimeRot );
-				
-				return getMatrix( lerpTrans, lerpRot );
-			}
-			
-			ci::mat4 getLooped( double absTime ) const
-			{
-				CI_ASSERT( jointTrans.size() >= 2 && jointRots.size() >= 2 );
-				auto cyclicTimeTrans = glm::mod( absTime, mTransDuration ) + mTransStartTime;
-				auto cyclicTimeRot = glm::mod( absTime, mRotDuration ) + mRotStartTime;
-				auto begItTrans = begin( jointTrans );
-				auto nextItTrans = std::upper_bound( begItTrans, end( jointTrans ) - 1, cyclicTimeTrans,
-				[]( float time, const TransKeyframe &val ){
-					return time < val.time;
-				});
-				
-				auto prevItTrans = nextItTrans - 1;
-				auto perTimeTrans = ( cyclicTimeTrans - prevItTrans->time) /
-				( nextItTrans->time - prevItTrans->time);
-				auto lerpTrans = glm::mix( prevItTrans->trans, nextItTrans->trans, perTimeTrans );
-				
-				auto begItRot = begin( jointRots );
-				auto nextItRot = std::upper_bound( begItRot, end( jointRots ) - 1, cyclicTimeRot,
-				[]( float time, const RotKeyframe &val ){
-					return time < val.time;
-				});
-				
-				auto prevItRot = nextItRot - 1;
-				auto perTimeRot = ( cyclicTimeRot - prevItRot->time) /
-				( nextItRot->time - prevItRot->time);
-				auto lerpRot = glm::slerp( prevItRot->rot, nextItRot->rot, (float)perTimeRot );
-				
-				auto ret = getMatrix( lerpTrans, lerpRot );
-				return ret;
-			};
-			
-			ci::mat4 getMatrix( const ci::vec3 &trans, const ci::quat &rot ) const
-			{
-				glm::mat4 ret;
-				ret *= glm::translate( trans );
-				ret *= glm::toMat4( rot );
-				return ret;
-			}
+			ci::mat4 get( double absTime ) const;
+			ci::mat4 getLooped( double absTime ) const;
+			ci::mat4 getMatrix( const ci::vec3 &trans, const ci::quat &rot ) const;
 			
 		private:
 			
@@ -263,20 +117,8 @@ public:
 			}
 		}
 		
-		void getLocal( double time, std::vector<ci::mat4> *localJointTransforms ) const
-		{
-			localJointTransforms->clear();
-			for( auto & jointClip : joints )
-				localJointTransforms->emplace_back( jointClip.get( time ) );
-		}
-		void getLoopedLocal( double time, std::vector<ci::mat4> *localJointTransforms ) const
-		{
-			localJointTransforms->clear();
-			for( auto & jointClip : joints ) {
-				auto ret = jointClip.getLooped( time );
-				localJointTransforms->emplace_back( ret );
-			}
-		}
+		void getLocal( double time, std::vector<ci::mat4> *localJointTransforms ) const;
+		void getLoopedLocal( double time, std::vector<ci::mat4> *localJointTransforms ) const;
 		
 	private:
 		std::vector<JointClip> joints;
@@ -358,17 +200,4 @@ inline void Skeleton::calcGlobalMatrices( const std::vector<ci::mat4> &localJoin
 	// Derive children
 	for( int i = 1, end = mJointArray.size(); i < end; i++ )
 		globalJoint->emplace_back( (*globalJoint)[mJointArray[i].getParentId()] * localJointTransforms[i] );
-}
-
-inline void Skeleton::Anim::getLocal( double time, std::vector<ci::mat4> *localJointTransforms ) const
-{
-	localJointTransforms->clear();
-	for( auto & jointClip : mJointClips )
-		localJointTransforms->emplace_back( jointClip.getMatrix( time ) );
-}
-inline void Skeleton::Anim::getLoopedLocal( double time, std::vector<ci::mat4> *localJointTransforms  ) const
-{
-	localJointTransforms->clear();
-	for( auto & jointClip : mJointClips )
-		localJointTransforms->emplace_back( jointClip.getMatrixLooped( time ) );
 }
